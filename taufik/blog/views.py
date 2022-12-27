d3ceb08d9a03529f9f8b8f47e7fe1a506d474325
Taufikzakariaa <117067737+Taufikzakariaa@users.noreply.github.com>
2022-12-27 11:24:49 +0700

    Add files via upload

---
+++ taufik/blog/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from .models import blog
+
+
+def index(request):
+    context = {
+        'content': 'selamat datang',
+    }
+    return render(request, 'index.html', context)
+
+
+def blogs(request):
+    blogs = blog.objects.all()
+
+    context = {
+        'blogs': blogs,
+    }
+    return render(request, 'home.html', context)
